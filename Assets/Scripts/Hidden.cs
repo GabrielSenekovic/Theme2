69c7d127c4ba443e5fad357ae8762c24cbaa0a90
Casper G <caspergustavsson5@gmail.com>
2022-02-18 19:29:56 +0100

    wood platform + hidden + anti + box + hazard bar
    removed collider from wooden beam
    hidden block can now be empty if you want
    difrent anti soft lock, doesnt bug.
    added box prefab
    added hazard bar

--- Assets/Scripts/Hidden.cs
+++ Assets/Scripts/Hidden.cs
@@ -6,7 +6,7 @@ public class Hidden : MonoBehaviour
 {
 
 
-    public Rigidbody2D rb;
+    //public Rigidbody2D rb;
     public GameObject contents;
     public bool showHitBox = false;
 
@@ -21,8 +21,11 @@ public class Hidden : MonoBehaviour
     {
         if(other.gameObject.tag == "Player" && other.gameObject.transform.position.y < transform.position.y - 0.5f && !hit) 
         {
-            GameObject obj = Instantiate<GameObject>(contents);
-            obj.transform.position = transform.position;
+            if(contents.gameObject != null)
+            {
+                GameObject obj = Instantiate<GameObject>(contents);
+                obj.transform.position = transform.position;
+            }
             GetComponent<SpriteRenderer>().enabled = showHitBox;
             hit = true;
             //Destroy(gameObject);
