1c503a5050a3b18f691510b845fd53d6b39a6d71
Gabriel <Gabriel.Senekovic@gmail.com>
2024-05-23 16:14:51 +0200

    Save file system finished (?)

--- Assets/Scripts/UI/SaveFileButton.cs
+++ Assets/Scripts/UI/SaveFileButton.cs
@@ -5,17 +5,25 @@ using TMPro;
 using static SaveFileSelect;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
 
 public class SaveFileButton : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI playerName;
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] Button deleteButton;
 
     SaveFile saveFile;
     StartMenu startMenu;
 
+    string saveFilePath;
+
     private void Awake()
     {
-        saveFile = new SaveFile();
+        inputField.gameObject.SetActive(false);
+        saveFilePath = Application.persistentDataPath + "/SaveFiles";
+        saveFile = null;
+        deleteButton.gameObject.SetActive(false);
     }
     public void Initialize(StartMenu startMenu)
     {
@@ -24,31 +32,48 @@ public class SaveFileButton : MonoBehaviour
     public void SetFile(SaveFile file)
     {
         saveFile = file;
+        saveFile.strings.TryGetValue("playerName", out string playerName);
+        this.playerName.text = playerName;
+        deleteButton.gameObject.SetActive(true);
     }
     public void OnClick()
     {
-        startMenu.LoadSave(saveFile);
-    }
-    public bool Load()
-    {
-        if (File.Exists(Application.persistentDataPath + "/saveFile.save"))
+        if(saveFile == null)
         {
-            Debug.Log("Save file found");
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/saveFile.save", FileMode.Open);
-            saveFile = (SaveFile)bf.Deserialize(file);
-            file.Close();
-            return true;
+            playerName.gameObject.SetActive(false);
+            inputField.gameObject.SetActive(true);
+            inputField.Select();
+            //Write name, create new save
         }
-        return false;
+        else
+        {
+            startMenu.LoadSave(saveFile);
+        }
     }
-    public bool Save()
+    public void CreateSaveFile()
+    {
+        playerName.text = inputField.text;
+        playerName.gameObject.SetActive(true);
+        inputField.gameObject.SetActive(false);
+
+        saveFile = new SaveFile();
+
+        saveFile.strings.Add("playerName", playerName.text);
+        deleteButton.gameObject.SetActive(true);
+        Save();
+    }
+    public void Save()
     {
         Debug.Log("Save file saved");
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/" + playerName.text + ".save");
+        FileStream file = File.Create(saveFilePath + "/" + playerName.text + ".save");
         bf.Serialize(file, saveFile);
         file.Close();
-        return true;
+    }
+    public void Delete()
+    {
+        File.Delete(saveFilePath + "/" + playerName.text + ".save");
+        deleteButton.gameObject.SetActive(false);
+        playerName.text = "New Game";
     }
 }
--- Assets/Scripts/UI/SaveFileSelect.cs
+++ Assets/Scripts/UI/SaveFileSelect.cs
@@ -4,23 +4,58 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveFileSelect : MonoBehaviour
 {
+    [SerializeField] GameObject areYouSure;
+    [SerializeField] Button yesImSure;
     [SerializeField] List<SaveFileButton> buttonList;
 
-    string saveFilePath = Application.persistentDataPath + "/SaveFiles";
+    string saveFilePath;
 
     private void Awake()
     {
+        areYouSure.SetActive(false);
+        saveFilePath = Application.persistentDataPath + "/SaveFiles";
+    }
+    private void Start()
+    {
         //load savefiles
-        string[] fileEntries = Directory.GetFiles(Application.persistentDataPath);
-        string[] subdirectoryEntries = Directory.GetDirectories(Application.persistentDataPath);
-        if(!Directory.Exists(saveFilePath))
+        string[] fileEntries = Directory.GetFiles(saveFilePath);
+        if (!Directory.Exists(saveFilePath))
         {
             Directory.CreateDirectory(saveFilePath);
-            subdirectoryEntries = Directory.GetDirectories(Application.persistentDataPath);
         }
+        for (int i = 0; i < fileEntries.Length; i++)
+        {
+            if (Load(fileEntries[i], out SaveFile saveFile))
+            {
+                buttonList[i].SetFile(saveFile);
+                continue;
+            }
+            break;
+        }
+    }
+    public void PressDelete(int i)
+    {
+        areYouSure.gameObject.SetActive(true);
+        yesImSure.onClick.RemoveAllListeners();
+        yesImSure.onClick.AddListener(() => { buttonList[i].Delete(); areYouSure.gameObject.SetActive(false); });
+    }
+    public bool Load(string value, out SaveFile saveFile)
+    {
+        saveFile = null;
+        if (File.Exists(value))
+        {
+            Debug.Log("Save file found");
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(value, FileMode.Open);
+            saveFile = (SaveFile)bf.Deserialize(file);
+            file.Close();
+            return true;
+        }
+        return false;
     }
     public void SetStartMenu(StartMenu startMenu)
     {
